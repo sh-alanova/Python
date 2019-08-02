2b9b26e9960247b3a2a823918f2a19624310a556
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-02 13:57:42 +0600

    Код задачи

---
+++ Lesson-4/Problem-4/main.py
@@ -0,0 +1,16 @@
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+x3 = int(input())
+y3 = int(input())
+x4 = int(input())
+y4 = int(input())
+left = max(x1, x3)
+right = min(x2, x4)
+down = max(y1, y3)
+up = min(y2, y4)
+if(left < right and down < up):
+    print((right - left) * (up - down))
+else:
+    print(0)
