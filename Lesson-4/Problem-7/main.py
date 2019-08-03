f26bd02a8450be427f27afcac38cf8b16908d904
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-03 15:19:48 +0600

    Код задачи

---
+++ Lesson-4/Problem-7/main.py
@@ -0,0 +1,8 @@
+i, j = map(int, input().split())
+if i <= 100 and j <= 100:
+    if i == 99 and j == 100 or j == 99 and i == 100:
+        print('98')
+    elif i < j:
+        print(i)
+    else:
+        print(j)
