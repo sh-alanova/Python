236f53aff6d46c5467f7595fb07d59086812ec12
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-04 12:46:47 +0600

    Проблема

---
+++ Lesson-6/Problem-3/main.py
@@ -0,0 +1,7 @@
+sum = 0
+while (n = int(input())) != 0:
+    sum += n
+print(sum)
+''' File "C:/Python/untitled-1.py", line 2, in <module>
+    while (n = int(input())) != 0:
+Syntax Error: invalid syntax: <string>, line 2, pos 10 '''
