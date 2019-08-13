942d6526c0ed3321d03f1f931817bff8b22030e8
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-13 19:44:17 +0600

    Код задачи

---
+++ Lesson-15/Problem-4/main.py
@@ -0,0 +1,18 @@
+a = int(input())
+b = int(input())
+IsPrime = [True] * (b + 1)
+IsPrime[0] = False
+IsPrime[1] = False
+d = 2
+while d * d <= b:
+    if IsPrime[d]:
+        for i in range(d * d, b + 1, d):
+            IsPrime[i] = False
+    d += 1
+Primes = []
+ans = 0
+for i in range(a, b + 1):
+    if IsPrime[i] == True:
+        Primes.append(i)
+        ans += 1
+print(ans)
