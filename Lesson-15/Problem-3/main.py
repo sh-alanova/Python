46c85c5f6a54f58afc0cfb135fa51ec5736709d0
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-13 19:44:11 +0600

    Код задачи

---
+++ Lesson-15/Problem-3/main.py
@@ -0,0 +1,19 @@
+n = 10 ** 7
+IsPrime = [True] * (n + 1)
+IsPrime[0] = False
+IsPrime[1] = False
+d = 2
+while d * d <= n:
+    if IsPrime[d]:
+        for i in range(d * d, n + 1, d):
+            IsPrime[i] = False
+    d += 1
+Primes = []
+ans = 0
+for i in range(0, n + 1):
+    if IsPrime[i] == True:
+        Primes.append(i)
+        ans += 1
+
+print(Primes)
+print(ans)
