36e297dfde863a6b6c3a0fe844c4d06e9913c12f
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-14 08:45:53 +0600

    Обновила

--- Lesson-4/Problem-5/main.py
+++ Lesson-4/Problem-5/main.py
@@ -2,13 +2,11 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
-if (c == 0 and d == 0) or a == 0 or (- b // a) == (- d // c):
+if a == 0 and b != 0:
     print('NO')
 elif a == 0 and b == 0:
     print('INF')
-elif(b % a == 0):
+elif a != 0 and b * c / a != d and b % a == 0:
     print(- b // a)
 else:
     print('NO')
-
-    
