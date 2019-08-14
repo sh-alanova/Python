36fe254c2aa979b3a139b57ebe7f79463698a53c
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-14 19:03:23 +0600

    Обновила

--- Lesson-8/Problem-7/main.py
+++ Lesson-8/Problem-7/main.py
@@ -1,11 +1,7 @@
 s = input().split()
 A = list(map(int, s))
-B = []
-for i in range(0, len(A)):
-    if A[i] > 0:
-        B.append(A[i])
-min = B[0]
-for i in range(0, len(B)):
-        if min > B[i]:
-            min = B[i]
-print(min)
+min_el = A[0]
+for elem in A:
+    if min_el > elem and elem > 0:
+        min_el = elem
+print(min_el)
