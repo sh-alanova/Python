147b2f776bed7a7c999005c49e94f99e259dc212
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-15 20:18:04 +0600

    Обновила

--- Lesson-7/Problem-5/main.py
+++ Lesson-7/Problem-5/main.py
@@ -1,9 +1,9 @@
 s = input()
 sum = 0
 k = 0
-for i in range(len(s)):
-    if '0' <= s[i] <= '9':
-        k = k * 10 + int(s[i])  
+for c in s:
+    if '0' <= c <= '9':
+        k = k * 10 + int(c)  
     else:
         sum += k
         k = 0
