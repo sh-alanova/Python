506ba8ecee84fcad269e84b24d06b925cfb9cd20
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-15 20:05:04 +0600

    Обновила

--- Lesson-4/Problem-8/main.py
+++ Lesson-4/Problem-8/main.py
@@ -1,11 +1,9 @@
 n = int(input())
 k = int(input())
-if(1 <= k <= n <= 50) and n % 2 == 0:
-    if k == n / 2:
-        print(0)
-    elif k < n / 2:
-        t = n // 2 - k
-        print(t)
-    else:
-        t =  n // 2 - (n - k + 1)
-        print(t)
+if n == 2:
+    ans = 0
+elif k <= n // 2:
+    ans = n - k - 2
+else:
+    ans = k - 3
+print(ans)
