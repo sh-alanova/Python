48d0a41eb269930fe79a58934bbb56aa69f14614
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-15 22:21:46 +0600

    Обновила

--- Lesson-8/Problem-7/main.py
+++ Lesson-8/Problem-7/main.py
@@ -1,7 +1,9 @@
 s = input().split()
 A = list(map(int, s))
-min_el = A[0]
+min_el = None
 for elem in A:
-    if min_el > elem and elem > 0:
+    if (min_el is None) and elem > 0:
+        min_el = elem
+    elif (min_el is not None) and min_el > elem and elem > 0:
         min_el = elem
 print(min_el)
