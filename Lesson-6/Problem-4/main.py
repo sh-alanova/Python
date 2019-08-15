ba3e521c172cb4e7d9db84620718880591fd4e0b
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-15 20:11:55 +0600

    Обновила

--- Lesson-6/Problem-4/main.py
+++ Lesson-6/Problem-4/main.py
@@ -2,9 +2,7 @@ n = int(input())
 min_dig = max_dig = n % 10
 while n > 0:
     digit = n % 10
-    if digit < min_dig:
-        min_dig = digit
-    elif digit > max_dig:
-        max_dig = digit
+    min_dig = min(digit, min_dig)
+    max_dig = max(digit, max_dig)
     n //= 10 
 print(min_dig, max_dig)
