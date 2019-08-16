ffb2e14335ac288408766113fa963b3f8d5c0da7
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-16 08:24:10 +0600

    Обновила

--- Lesson-11/Problem-4/main.py
+++ Lesson-11/Problem-4/main.py
@@ -1,10 +1,13 @@
+import sys
+sys.setrecursionlimit(5000)
+
 def A(m, n):
     if m == 0:
         return n + 1
     elif n == 0:
         return A(m - 1, 1)
     else:
-        return A(m - 1, (m, n - 1))
+        return A(m - 1, A(m, n - 1))
 
 m = int(input())
 n = int(input())
