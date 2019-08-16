691c2e902454da624013aa4fa7fe4f3ca6424abd
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-16 20:27:35 +0600

    Код задачи

--- Lesson-12/Problem-4/main.py
+++ Lesson-12/Problem-4/main.py
@@ -1,1 +1,22 @@
-
+def Hanoi(n, st, fn):
+    tmp = 6 - st - fn
+    if n == 1:
+        if fn == st % 3 + 1:
+            print(n, st, fn)
+        else:
+            print(n, st, tmp)
+            print(n, tmp, fn)
+    else:
+        if fn == st % 3 + 1:
+            Hanoi(n - 1, st, tmp)
+            print(n, st, fn)
+            Hanoi(n - 1, tmp, fn)
+        else:
+            Hanoi(n - 1, st, fn)
+            print(n, st, tmp)
+            Hanoi(n - 1, fn, st)   
+            print(n, tmp, fn);
+            Hanoi(n - 1, st, fn)
+            
+n = int(input())
+Hanoi(n, 1, 3)
