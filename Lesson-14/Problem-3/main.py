c52fb2a4557a6e11fcef094a98c18a5214344a4e
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-17 16:58:12 +0600

    Обновила

--- Lesson-14/Problem-3/main.py
+++ Lesson-14/Problem-3/main.py
@@ -1,17 +1,7 @@
-def gcd(a, b):
-    global ans
-    while a != 0 and b != 0:
-        ans += a // b
-        a = a % b
-        a, b = b, a
-    return ans
-    
 a = int(input())
 b = int(input())
 ans = 0
-if max(a, b) % min(a, b) == 0:
-    print(max(a, b) // min(a, b))
-else:
-    print(gcd(a, b))
-    
-            
+while b != 0:
+    ans += a // b
+    a, b = b, a % b
+print(ans)
