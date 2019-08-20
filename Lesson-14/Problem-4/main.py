f9f0a0d8c9439987f317d7a0bb29c757f5caa8b2
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-20 19:34:46 +0600

    Обновила

--- Lesson-14/Problem-4/main.py
+++ Lesson-14/Problem-4/main.py
@@ -1,19 +1,16 @@
 def gcd(a, b):
-    if a > 0 and b > 0:
-        while a != 0 and b != 0:
-            a = a % b
-            a, b = b, a
-        return max(a, b)
-    else:
-        return 0
-      
+    while b != 0:
+        a, b = b, a % b
+    return a
 x1 = int(input())
 y1 = int(input())
 x2 = int(input())
 y2 = int(input())
-if x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0: 
-    print(abs((abs(x2) - abs(x1))) + abs((abs(y2) - abs(y1))) - gcd(abs((abs(x2) - abs(x1))), abs((abs(x2) - abs(x1)))))
+a = abs(x2 - x1)
+b = abs(y2 - y1)
+if a == 0 or b == 0:
+    print(0)
 else:
-    print(abs(x2 - x1) + abs(y2 - y1) - gcd(abs(x2 - x1), abs(y2 - y1)))
+    ans = a + b - gcd(a, b)
+    print(ans)
     
-            
