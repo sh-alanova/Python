9e9b7cc846931474433d2696c19ac8e11f0c932f
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-22 21:28:14 +0600

    Обновила

--- Lesson-14/Problem-5/main.py
+++ Lesson-14/Problem-5/main.py
@@ -1,11 +1,8 @@
 def gcd(a, b):
-    if b == 0:
-        return a
-    else:
-        while b != 0:
-            a, b = b, a % b
-        return a
-      
+    while b != 0:
+        a, b = b, a % b
+    return a
+
 a = int(input())
 b = int(input())
 n = int(input())
