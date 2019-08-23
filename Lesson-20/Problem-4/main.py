544fbb61fcd9e5fc2e993a1e0d90f317b222fb90
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-23 22:17:24 +0600

    Код задачи

---
+++ Lesson-20/Problem-4/main.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = [0] * n
+for i in range(n):
+    B[i] = (A[i], i)
+B.sort(reverse = True)
+sum = 0
+for i in range(n):
+    sum += B[i][0] * (i + 1)
+print(sum)
+for i in range(n):
+    print(B[i][1] + 1, end = ' ')
