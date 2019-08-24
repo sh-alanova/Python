8f96a635510d1f9e74bca0ee19b17228efc2e439
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-24 11:54:06 +0600

    Проходит 25 из 32 тестов

--- Lesson-17/Problem-5/main.py
+++ Lesson-17/Problem-5/main.py
@@ -1,1 +1,23 @@
+def Sort(A):
+    count = [0] * 101
+    for elem in A:
+        count[elem] += 1
+    A[:] = []
+    for i in range(100):
+        A += [i] * count[i]
+    return A
 
+Boot = list(map(int, input().split()))
+People = list(map(int, input().split()))
+Sort(Boot)
+Sort(People)
+i = j = ans = 0
+while i < len(People) and j < len(Boot):
+    if People[i] <= Boot[j]:
+        i += 1
+        j += 1
+        ans += 1
+    else:
+        j += 1
+print(ans)
+    
