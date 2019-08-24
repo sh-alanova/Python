c242409169e111cd919ae049ef3ba48397899629
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-24 09:00:58 +0600

    Обновила

--- Lesson-16/Problem-4/main.py
+++ Lesson-16/Problem-4/main.py
@@ -1,23 +1,20 @@
-def BubbleSort(A): 
+def BubbleSort(A, s): 
     ans = 0
     for j in range(len(A) - 1, 0, -1): 
-        for i in range(0, j): 
-            if A[i] > A[i + 1]: 
-                A[i], A[i + 1] = A[i + 1], A[i]
+        if s == 'ascending':
+            for i in range(0, j): 
+                if A[i] > A[i + 1]: 
+                    A[i], A[i + 1] = A[i + 1], A[i]
+        else:
+            for i in range(0, j): 
+                if A[i] < A[i + 1]: 
+                    A[i], A[i + 1] = A[i + 1], A[i]                 
     return A
-
-def UnbubbleSort(B): 
-    ans = 0
-    for j in range(len(B) - 1, 0, -1): 
-        for i in range(0, j): 
-            if B[i] < B[i + 1]: 
-                B[i], B[i + 1] = B[i + 1], B[i]
-    return B
 
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-BubbleSort(A)
-UnbubbleSort(B)
+BubbleSort(A, 'ascending')
+BubbleSort(B, 'descending')
 ans = 0
 for i in range(len(A)):
     ans += A[i] * B[i]
