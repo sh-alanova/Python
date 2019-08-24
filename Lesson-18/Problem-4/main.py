220a5521315a7d86227265210eb240eb90c6e506
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-24 11:59:24 +0600

    Проходит 57 из 65 тестов

--- Lesson-18/Problem-4/main.py
+++ Lesson-18/Problem-4/main.py
@@ -1,1 +1,21 @@
-
+n = int(input())
+way1 = list(map(int, input().split()))
+way2 = []
+tup = []
+k = 1
+for i in range(n + 3):
+    if len(tup) > 0 and tup[-1] == k:
+        way2.append(tup.pop())
+        k += 1
+    elif len(way1) > 0 and way1[0] == k:
+        way2.append(way1.pop(0))
+        k += 1
+    elif len(way1) > 0:
+        tup.append(way1.pop(0))
+if len(way2) == n:
+    print('YES')
+else:
+    print('NO')
+        
+    
+    
