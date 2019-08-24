85f113e09cbd626788bafc0c8a5b80d736c52b59
sh-alanova <38557996+sh-alanova@users.noreply.github.com>
2019-08-24 08:04:44 +0600

    Обновила

--- Lesson-10/Problem-3/main.py
+++ Lesson-10/Problem-3/main.py
@@ -1,28 +1,17 @@
-def Bublesort(A):
-    for i in range(len(A) - 1, 0, -1):
-        for j in range(0, i):
-            if A[j] > A[j + 1]:
-                A[j], A[j + 1] = A[j + 1], A[j]
-                                
 f = open('input.txt')
 g = open('output.txt', 'w')
 A = f.read().split()
 D = {}
 for elem in A:
-    D[elem] = 0
-for elem in A:
+    if elem not in D:
+        D[elem] = 0
     D[elem] += 1
 max_value = 0
 max_key = ''
 for key in D:
-    if max_value < D[key]:
+    if max_value < D[key] or (max_value == D[key] and max_key > key):
         max_value = D[key]
         max_key = key
-if max_value == 1:
-    Bublesort(A)
-    g.write(A[0])
-else:
-    g.write(max_key)
-    
+g.write(max_key)
 f.close()
 g.close()     
